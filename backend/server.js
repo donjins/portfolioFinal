bedbd2623a032035a324306c91e8630cde819a44
JOSEPH DON JINS <josephdon.dev@gmail.com>
2025-03-14 09:43:02 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,8 +8,8 @@ dotenv.config();
 const app = express();
 
 const PORT = process.env.PORT || 5000;
-const MONGO_URI = process.env.MONGO_URI || "mongodb+srv://josephdondev:CudR7KOYLa72D4HI@cluster0.mongodb.net/JosephPortfolioDB?retryWrites=true&w=majority
-";
+const MONGO_URI = process.env.MONGO_URI || "mongodb+srv://josephdondev:CudR7KOYLa72D4HI@cluster0.mongodb.net/JosephPortfolioDB?retryWrites=true&w=majority";
+
 
 // Middleware
 app.use(
