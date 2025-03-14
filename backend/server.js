9638f526210f17db667bf533d61846545550e08c
JOSEPH DON JINS <josephdon.dev@gmail.com>
2025-03-14 10:03:10 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -29,10 +29,7 @@ app.use("/uploads", express.static(path.join(__dirname, "uploads"))); // Serve s
 
 // Connect to MongoDB
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("✅ MongoDB Connected"))
   .catch((err) => console.error("❌ MongoDB Connection Error:", err));
 
